851f99f004a7c0710e9c7d5341c3f4310b48e32a
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-09-16 15:29:17 +0500

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,36 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="">
+        <div
+          className="flex
+        items-center
+        justify-between"
+        >
           <div className="mx-7">
             <h4 className="text-4xl uppercase font-bold">
               M<span className="text-teal-600">T</span>
             </h4>
           </div>
-          <div className="">
-            <ul className="">
+          <div
+            className="text-gray-900 
+          md:block
+          hidden
+          px-7
+          py-7
+          font-medium
+          bg-white
+          rounded-bl-full"
+          >
+            <ul
+              className="flex
+            items-senter
+            gap-1
+            py-2
+            text-lg"
+            >
               {menuLink?.map((menu, index) => (
                 <li
-                  className="px-6
+                  className="px-7
                 hover:text-teal-600"
                   key={index}
                 >
