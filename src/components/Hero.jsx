1e66b0ffed2a9d96d663d852d3f5a04d7e8ef237
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-09-29 17:05:23 +0500

    Update the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import HeroImage from "../assets/images/Hero_image.png";
 
 const Hero = () => {
   return (
@@ -8,7 +9,27 @@ const Hero = () => {
         className={`min-h-screen flex py-10 
       md:flex-row flex-col items-center`}
       >
-        Hero
+        <div className="flex-1 flex items-center justify-center h-full">
+          <img
+            src={HeroImage}
+            alt="HeroImgage"
+            className={`md:w-11/12 h-full object-cover rounded-full`}
+          />
+        </div>
+        <div className="flex-1">
+          <div className="">
+            <h1 className="">
+              <span>
+                Hello!
+                <br />
+              </span>
+              My name is <span className="">Muhammad Talha</span>
+            </h1>
+            <h4 className="">Fullstack Developer</h4>
+            <button className="">Contact Me</button>
+          </div>
+          <div className=""></div>
+        </div>
       </section>
     </>
   );
