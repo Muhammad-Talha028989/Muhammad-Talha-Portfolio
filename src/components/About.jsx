3bf8a9d66f6f1c716314db77ca439f464f6c57ba
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-09-29 17:57:27 +0500

    Adding amd Updating about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <section className={`py-10 text-white`} id="about">
+      <div className="text-center mt-g">
+        <h3 className="text-4xl font-semibold text-slate-700">
+          About<span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 my-3 text-lg">My introduction</p>
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+          <div className="">
+            <div className="text-slate-600 my-3">
+              <p className="text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eveniet, quisquam quibusdam! Et nihil voluptatem quibusdam quod
+                ratione, voluptatum iure totam minima ducimus eligendi ratione,
+                voluptatum iure totam minima ducimus eligendi ratione,
+                voluptatum iure totam minima ducimus eligendi praesentium aut
+                sunt, voluptates sed quos excepturi.
+              </p>
+              <div className="">
+                <div className="">
+                  <h3 className="">04+</h3>
+                  {setTimeout(() => {}, 1000)}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
---
+++ src/components/api/about.api.js
@@ -0,0 +1,16 @@
+export const infoAbout = [
+  {
+    text: "Years experience",
+    count: 04,
+  },
+  {
+    text: "Completed Projects",
+    count: 05,
+  },
+  {
+    text: "Companies Work",
+    count: 0,
+  },
+];
+
+export default infoAbout;
