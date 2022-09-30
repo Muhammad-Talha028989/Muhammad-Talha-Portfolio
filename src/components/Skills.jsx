23ddfb6e7e0fe3c3463b5fc773c45b42f8a7ae94
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-09-30 16:07:15 +0500

    Added the skill section and finished it

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { skills } from "./api/Skills.api";
 
 const Skills = () => {
-  return <div></div>;
+  return (
+    <section id="skills" className={`py-10 bg-gray-800 relative`}>
+      <div className="mt-8 text-slate-600 text-center">
+        <h3 className="text-4xl  font-semibold">
+          My <span className="text-cyan-600">Skills</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg">My knowledge</p>
+        <div className="flex items-center justify-center mt-12 gap-10 flex-wrap">
+          {skills?.map((skill, i) => (
+            <div
+              className="border-2 group  border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-16 rounded-xl"
+              key={i}
+            >
+              <div
+                style={{
+                  background: `conic-gradient(rgb(8,145,170) ${skill?.count}%, #ddd ${skill?.count}%)`,
+                }}
+                className="w-32 h-32 flex items-center justify-center rounded-full"
+              >
+                <div className="text-6xl text-white w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
+                  <ion-icon name={skill?.logo}></ion-icon>
+                </div>
+              </div>
+              <p className="text-xl mt-3 text-white">{skill?.level}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
---
+++ src/components/api/Skills.api.js
@@ -0,0 +1,8 @@
+export const skills = [
+  { logo: "logo-html5", level: "Advance", count: 86 },
+  { logo: "logo-css3", level: "Intermediate", count: 40 },
+  { logo: "logo-nodejs", level: "Expect", count: 93 },
+  { logo: "logo-react", level: "Expect", count: 95 },
+];
+
+export default skills;
