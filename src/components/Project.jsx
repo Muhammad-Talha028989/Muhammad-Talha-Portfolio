35b9fccc23f20cecaf0e3edfa60e609966930a6b
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-10-01 14:34:22 +0500

    Finished the Projects section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import ProjectImage from "../assets/images/projectCvcraft.png";
+import ProjectAuther from "../assets/images/Hero_image.png";
+import { projects } from "./api/Projects.api";
+
 const Project = () => {
   return (
     <section id={"projects"} className={"py-10 text-white"}>
@@ -10,9 +19,52 @@ const Project = () => {
         <p className="text-gray-600 mt-3 text-lg">My awesome works</p>
       </div>
       <br />
-      <div className="flex max-w-6xl px-5 mx-auto items-center relative">
+      <div className="flex max-w-6xl px-5 mx-auto items-center relative justify-center text-center">
         <div className="lg:w-2/3 w-full">
-          
+          <Swiper
+            className={""}
+            slidesPerview={1.5}
+            spaceBetween={10}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+            }}
+            loop={true}
+            autoplay={{
+              delay: 3000,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination, Autoplay]}
+          >
+            {projects?.map((content, index) => (
+              <SwiperSlide key={index}>
+                <div className="h-fit w-fit p-4 bg-slate-700 rounded-xl text-center ">
+                  <img src={content?.img?.ProjectsImages} alt="" className="rounded-lg" />
+                  <h3 className="text-xl my-4">{content?.name}</h3>
+                  <div className="flex gap-3 justify-center items-center">
+                    <a
+                      href={content?.github_link}
+                      className="hover:text-white text-cyan-600 bg-slate-800 px-2 py-1 inline-block"
+                    >
+                      Github
+                    </a>
+                    <a
+                      href={content?.live_link}
+                      className="hover:text-white text-cyan-600  bg-slate-800 px-2 py-1 inline-block"
+                    >
+                      Live Demo
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="lg:block hidden">
+          {/* <img src={ProjectAuther} alt="" /> */}
         </div>
       </div>
     </section>
---
+++ src/components/api/Projects.api.js
@@ -0,0 +1,22 @@
+import ProjectsImages from "../../assets/images/projectCvcraft.png";
+export const projects = [
+  {
+    img: { ProjectsImages },
+    name: "CVCrafts",
+    github_link: "",
+    live_link: "",
+  },
+  {
+    img: { ProjectsImages },
+    name: "CVCrafts",
+    github_link: "",
+    live_link: "",
+  },
+  {
+    img: { ProjectsImages },
+    name: "CVCrafts",
+    github_link: "",
+    live_link: "",
+  },
+];
+export default projects;
